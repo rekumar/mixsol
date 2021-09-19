4824375fd2fdcfb4f36cb6e4e76f3845db3c05c1
Rishi E Kumar <47284667+rekumar@users.noreply.github.com>
2021-09-19 16:58:53 -0700

    mixing working + powders

--- mixsol/mixsol.py
+++ mixsol/mixsol.py
@@ -1,21 +1,88 @@
 import numpy as np
-import networkx as nx
 from scipy.optimize import nnls
 from mixsol.solution import Solution
+import itertools as itt
+import matplotlib.pyplot as plt
 
 
-class SolutionMixer:
-    def __init__(self, stock_solutions: list, targets: dict = {}):
+class DirectedGraph:
+    def __init__(self, adjacency_matrix):
+        self.g = adjacency_matrix
+
+    def indegree(self, node):
+        return sum(self.g[node] > 0)
+
+    def children(self, node):
+        return [n for n, v in enumerate(self.g[:, node]) if v > 0]
+
+    def hierarchy(self, reverse=False):
+        hier = [h for h in self._hierarchy_iter()]
+        if reverse:
+            return hier[::-1]
+        else:
+            return hier
+
+    def _hierarchy_iter(self):
+        indegree_map = {}
+        zero_indegree = []
+        for node in range(self.g.shape[0]):
+            d = self.indegree(node)
+            if d > 0:
+                indegree_map[node] = d
+            else:
+                zero_indegree.append(node)
+
+        while zero_indegree:
+            this_generation = zero_indegree
+            zero_indegree = []
+            for node in this_generation:
+                for child in self.children(node):
+                    indegree_map[child] -= 1
+                    if indegree_map[child] == 0:
+                        zero_indegree.append(child)
+                        del indegree_map[child]
+            yield this_generation
+
+    def _normalize(self, adjacency_matrix):
+        v_in = adjacency_matrix.sum(axis=1)
+        selfidx = np.where(v_in == 0)[0]
+        v_in[selfidx] = 1
+        g_norm = adjacency_matrix / v_in[:, np.newaxis]
+        g_norm[selfidx, selfidx] = 1
+        return g_norm
+
+    def propagate_load(self, load):
+        if len(load) != self.g.shape[0]:
+            raise ValueError("load must have same number of elements as graph nodes!")
+        g_norm = self._normalize(self.g)
+
+        first_gen = self.hierarchy()[0]
+        for gen in self.hierarchy(reverse=True):
+            for node in gen:
+                needed = g_norm[node] * load[node]
+                if node in first_gen:
+                    load[node] = needed[node]
+                else:
+                    load = load + needed
+        return load
+
+
+class Mixer:
+    def __init__(self, stock_solutions: list, targets: dict = None):
+        if targets is None:
+            targets = {}
         self.target_solutions = list(targets.keys())
-        self.target_volumes = targets  # dict of solution:volume
-        self.solutions = stock_solutions + self.target_solutions
+        self.solutions = list(set(stock_solutions + self.target_solutions))
+        self.target_volumes = {
+            solution: targets.get(solution, 0) for solution in self.solutions
+        }
         (
             self.solution_matrix,
             self.solvent_idx,
             self.components,
-        ) = self._solutions_to_matrix(stock_solutions + self.target_solutions)
+        ) = self._solutions_to_matrix(self.solutions)
         self.stock_idx = [
-            i for i in range(len(stock_solutions))
+            self.solutions.index(stock) for stock in stock_solutions
         ]  # rows of solution matrix that belong to stock solutions
 
     def _solutions_to_matrix(self, solutions: list, components: list = None):
@@ -67,6 +134,11 @@ class SolutionMixer:
                 target_matrix[m] = target.solvent_dict[c] * volume
         return target_matrix.T
 
+    def _is_plausible(self, solution_matrix, target_vector):
+        """Check if the solution_matrix spans target vector values. If not, no chance of a successful mixture here!"""
+        difference = solution_matrix - target_vector
+        return not any([all(v > 0) or all(v < 0) for v in difference.T])
+
     def _calculate_mix(
         self, target, solution_indices: list = None, tolerance=1e-3, min_fraction=0
     ):
@@ -74,6 +146,8 @@ class SolutionMixer:
             solution_indices = list(range(len(self.solutions)))
         A = self.solution_matrix[solution_indices]
         b = self._solution_to_vector(target)
+        if not self._is_plausible(A, b):
+            return np.nan
         x, err = nnls(A.T, b, maxiter=1e3)
         x[x < 1e-10] = 0
         if err > tolerance:
@@ -91,9 +165,10 @@ class SolutionMixer:
         target,
         volume,
         solution_indices=None,
-        tolerance=1e-3,
+        tolerance=1e-2,
         min_volume=0,
         verbose=False,
+        max_inputs=None,
     ):
         min_fraction = min_volume / volume
         if solution_indices is None or solution_indices == "stock":
@@ -102,7 +177,9 @@ class SolutionMixer:
             solution_indices = list(range(len(self.solutions)))
 
         possible_mixtures = []
-        for i in range(1, len(self.solutions)):
+        if max_inputs is None:
+            max_inputs = len(self.solutions)
+        for i in range(1, max_inputs):
             for idx in itt.combinations(solution_indices, i):
                 x = self._calculate_mix(
                     target,
@@ -121,15 +198,21 @@ class SolutionMixer:
             return possible_mixtures * volume
         return possible_mixtures[0] * volume
 
-    def mixture_graph(self, min_volume=20):
-        self.graph = np.zeros((len(self.solutions), len(self.solutions)))
+    def _solve_adjacency_matrix(self, min_volume, max_inputs, max_generations):
+        graph = np.zeros((len(self.solutions), len(self.solutions)))
         self.availableidx = self.stock_idx.copy()
-        # self.graph[np.diag_indices(len(self.solutions))] = 1
-        attempts = 0
-        while len(self.availableidx) < len(self.solutions):
-            if attempts > 5:
+        generation = 0
+        n_remaining = len(self.solutions) - len(self.availableidx)
+        n_remaining_lastiter = np.inf
+        while n_remaining > 0:
+            if generation > max_generations or n_remaining == n_remaining_lastiter:
                 print("Could not find a solution")
+                for i, s in enumerate(self.solutions):
+                    if i not in self.availableidx:
+                        print(s)
                 break
+            n_remaining_lastiter = n_remaining
+
             for i in range(len(self.solutions)):
                 if i in self.availableidx:
                     continue
@@ -138,9 +221,171 @@ class SolutionMixer:
                     volume=self.target_volumes[self.solutions[i]],
                     solution_indices=self.availableidx,
                     min_volume=min_volume,
+                    max_inputs=max_inputs,
                 )
                 if not np.isnan(x).any():
-                    self.graph[i, :] = x
+                    graph[i, :] = x
                     self.availableidx.append(i)
-            attempts += 1
-        # return self.graph
+                    n_remaining -= 1
+            generation += 1
+        return graph
+
+    def solve(self, min_volume, max_inputs=None, max_generations=np.inf):
+        if max_inputs is None:
+            max_inputs = len(self.solutions) - 1
+
+        adjacency_matrix = self._solve_adjacency_matrix(
+            min_volume=min_volume,
+            max_inputs=max_inputs,
+            max_generations=max_generations,
+        )
+        self.graph = DirectedGraph(adjacency_matrix)
+        g_norm = self.graph._normalize(self.graph.g)
+        v_end = np.array([self.target_volumes.get(soln, 0) for soln in self.solutions])
+        v_needed = self.graph.propagate_load(v_end)
+        self.mixing_order = []
+        for generation in self.graph.hierarchy():
+            mixes_in_this_gen = {}
+            for solution_index in generation:
+                this_mix = {}
+                for input_solution, input_fraction in zip(
+                    self.solutions, g_norm[solution_index]
+                ):
+                    if input_fraction > 0:
+                        this_mix[input_solution] = (
+                            v_needed[solution_index] * input_fraction
+                        )
+                mixes_in_this_gen[self.solutions[solution_index]] = this_mix
+            self.mixing_order.append(mixes_in_this_gen)
+
+        transfers = {}
+        self.stock_volumes = {}
+        for gen in self.mixing_order:
+            for node, sources in gen.items():
+                for source, volume in sources.items():
+                    if source == node:
+                        self.stock_volumes[node] = volume
+                        continue
+                    if source not in transfers:
+                        transfers[source] = {}
+                    transfers[source][node] = volume
+        self.transfers_per_generation = []
+        for gen in self.mixing_order:
+            this_gen = {node: transfers[node] for node in gen if node in transfers}
+            if len(this_gen) > 0:
+                self.transfers_per_generation.append(this_gen)
+
+    ### publishing methods
+    def print(self):
+        # for idx in self.stock_idx:
+        #     volume_graph[idx, idx] = self.target_volumes.get(self.solutions[idx], 0)
+        print("===== Stock Prep =====")
+        for solution, volume in self.stock_volumes.items():
+            print(f"{volume:.2f} of {solution}")
+        print(f"====== Mixing =====")
+        for generation in self.transfers_per_generation:
+            for source, transfers in generation.items():
+                print(f"Distribute {source}:")
+                for destination, volume in transfers.items():
+                    print(f"\t{volume:.2f} to {destination}")
+
+    def plot(self):
+        nodes = {}
+        fig, ax = plt.subplots(figsize=(10, 10))
+        ax.set_aspect("equal")
+        for i, gen in enumerate(self.mixing_order):
+            xoff = 0.5 * (i % 2) + np.random.random() * 0.2
+            for j, (node, sources) in enumerate(gen.items()):
+                if node not in nodes:
+                    x = j * 1.25 + xoff * len(self.mixing_order)
+                    y = i
+                    c = plt.cm.Set2(i)
+                    nodes[node] = (x, y, c)
+                else:
+                    x, y, c = nodes[node]
+                for source, volume in sources.items():
+                    if source == node:
+                        x1 = x + 0.1 * len(self.mixing_order)
+                        y1 = y
+                        rotation = 0
+                        ha = "left"
+                        _, _, c = nodes[source]
+                    else:
+                        x1, y1, c = nodes[source]
+                        np.rad2deg(np.arctan((y - y1) / (x - x1)))
+                        ha = "center"
+                        ax.arrow(
+                            x=x1,
+                            y=y1,
+                            dx=x - x1,
+                            dy=y - y1,
+                            length_includes_head=True,
+                            head_width=0.1,
+                            color="k",
+                            alpha=0.5,
+                        )
+
+                    factor = 0.25 + 0.5 * np.random.random()
+                    plt.text(
+                        x=x + (x1 - x) * factor,
+                        y=y + (y1 - y) * factor,
+                        s=f"{volume:.2f}",
+                        ha=ha,
+                        va="center",
+                        rotation=rotation,
+                        bbox=dict(boxstyle="round", fc=c),
+                    )
+                plt.scatter(x, y, color=c, s=300, zorder=999, alpha=0.5)
+                plt.text(
+                    x=x,
+                    y=y,
+                    s=str(node),
+                    ha="center",
+                    va="center",
+                    weight="bold",
+                    zorder=1000,
+                )
+
+        plt.tight_layout()
+
+
+class SolutionMaker:
+    def __init__(self, powders: list):
+        self.powders = powders
+        self.matrix, self.components = self._powders_to_matrix(powders)
+
+    def _powders_to_matrix(self, powders: list):
+        # get possible solution components from stock list
+        components = set()
+        for p in powders:
+            components.update(p.components.keys())
+        components = list(
+            components
+        )  # sets are not order-preserving, lists are - just safer this way
+
+        # organize components into a stock matrix, keep track of which rows are solvents
+        solid_matrix = np.zeros((len(powders), len(components)))
+        for m, p in enumerate(powders):
+            for n, c in enumerate(components):
+                solid_matrix[m, n] = p.components.get(c, 0)
+        return solid_matrix, components
+
+    def _solution_to_vector(self, target: Solution, volume=1):
+        # organize target solution into a matrix of total mols desired of each component
+        target_matrix = np.zeros((len(self.components),))
+        for m, c in enumerate(self.components):
+            if c in target.solute_dict:
+                target_matrix[m] = target.solute_dict[c] * target.molarity * volume
+        return target_matrix.T
+
+    def _calculate_mix(self, target, tolerance=1e-3):
+        x, err = nnls(self.matrix.T, target, maxiter=1e3)
+        x[x < 1e-10] = 0
+        if err > tolerance:
+            return np.nan
+        return x
+
+    def get_weights(self, target: Solution, volume: float):
+        target_vector = self._solution_to_vector(target, volume)
+        mass_vector = self._calculate_mix(target_vector)
+        return {str(c): m for c, m in zip(self.powders, mass_vector) if m > 0}
--- mixsol/solution.py
+++ mixsol/solution.py
@@ -1,9 +1,16 @@
+import numpy as np
+from molmass import Formula
+
 #### name parsing helper functions
 def components_to_name(components, delimiter="_"):
     composition_label = ""
     for c, n in components.items():
-        if n > 0:
-            composition_label += "{0}{1:.2f}{2}".format(c, n, delimiter)
+        if n == 0:
+            continue
+        elif n == 1:
+            composition_label += "{0}{2}".format(c, n, delimiter)
+        else:
+            composition_label += "{0}{1:.3g}{2}".format(c, n, delimiter)
 
     return composition_label[:-1]
 
@@ -60,3 +67,108 @@ def solutions_to_matrix(solutions: list):
     solvent_idx = list(solvent_idx)
 
     return stock_matrix
+
+
+def calculate_molar_mass(formula, delimiter="_"):
+    try:
+        return Formula(formula.replace(delimiter, "")).mass
+    except:
+        raise ValueError(
+            f"Could not guess the molar mass for formula {formula}. Maybe there are non-elemental formula units?\n Either replace all formula units with elemental components, or manually input using the molar_mass argument."
+        )
+
+
+class Solution:
+    def __init__(
+        self, solvent: str, solutes: str = "", molarity: float = 0, alias: str = None
+    ):
+        if solutes != "" and molarity == 0:
+            raise ValueError(
+                "If the solution contains solutes, the molarity must be >0!"
+            )
+        if solutes == "":
+            molarity = 1
+
+        self.molarity = molarity
+        self.solute_dict = name_to_components(solutes, factor=molarity, delimiter="_")
+        self.solutes = components_to_name(self.solute_dict, delimiter="_")
+        self.solvent_dict = name_to_components(solvent, factor=1, delimiter="_")
+        self.solvent = components_to_name(self.solvent_dict, delimiter="_")
+        total_solvent_amt = sum(self.solvent_dict.values())
+        self.solvent_dict = {
+            k: v / total_solvent_amt for k, v in self.solvent_dict.items()
+        }  # normalize so total solvent amount is 1.0
+
+        self.alias = alias
+
+    def to_dict(self):
+        out = {
+            "solutes": self.solutes,
+            "molarity": self.molarity,
+            "solvent": self.solvent,
+            "well": self.well,
+        }
+        return out
+
+    def to_json(self):
+        return json.dumps(self.to_dict())
+
+    def __str__(self):
+        if self.alias is not None:
+            return self.alias
+        if self.solutes == "":  # no solutes, just a solvent
+            return f"{self.solvent}"
+        return f"{round(self.molarity,2)}M {self.solutes} in {self.solvent}"
+
+    def __repr__(self):
+        return f"<Solution>" + str(self)
+
+    def __eq__(self, other):
+        if isinstance(other, self.__class__):
+            return (
+                self.solutes == other.solutes
+                and self.molarity == other.molarity
+                and self.solvent == other.solvent
+            )
+        else:
+            return False
+
+    def __key(self):
+        return (self.solutes, self.molarity, self.solvent)
+
+    def __hash__(self):
+        return hash(self.__key())
+
+
+class Powder:
+    def __init__(self, formula: str, molar_mass: float = None, alias: str = None):
+        if molar_mass is None:
+            self.molar_mass = calculate_molar_mass(formula, "_")
+        else:
+            self.molar_mass = molar_mass
+        self.formula = formula
+        self.components = name_to_components(
+            formula, factor=1 / self.molar_mass, delimiter="_"
+        )
+        self.alias = alias
+
+    def __str__(self):
+        if self.alias is not None:
+            return self.alias
+        else:
+            return self.formula
+
+    def __repr__(self):
+        return f"<Powder>" + str(self)
+
+    def __eq__(self, other):
+        if isinstance(other, self.__class__):
+            return self.formula == other.formula
+        else:
+            return False
+
+    def __key(self):
+        return (self.formula, self.molar_mass)
+
+    def __hash__(self):
+        return hash(self.__key())
