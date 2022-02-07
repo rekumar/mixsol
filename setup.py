fa7fbed7a39103f3085d55f7efdf1d23974b58ea
Rishi E Kumar <47284667+rekumar@users.noreply.github.com>
2022-02-07 11:07:53 -0800

    move to 0.6

--- setup.py
+++ setup.py
@@ -3,13 +3,13 @@ from distutils.core import setup
 setup(
     name="mixsol",
     packages=["mixsol"],
-    version="0.5",
+    version="0.6",
     license="GPLv3",
     description="Planning tool for combinatorial solution mixing. Reach target solutions from mixes of starting solutions, constrained by minimum pipetting volumes. Also aids in computing amounts of powdered reagents required to form solutions with target solutes + molarities.",
     author="Rishi E Kumar",
     author_email="rishi42@gmail.com",
     url="https://github.com/rekumar/mixsol",
-    download_url="https://github.com/rekumar/mixsol/archive/refs/tags/v0.5.tar.gz",
+    download_url="https://github.com/rekumar/mixsol/archive/refs/tags/v0.6.tar.gz",
     keywords=[
         "Chemistry",
         "Mixing",
